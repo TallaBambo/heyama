8ac00177b8288c0d8254a1fb4dad3039ccba5c38
TallaBambo <tallabambo041@gmail.com>
2026-05-08 11:15:55 +0100

    Heyama test backend

--- heyama-frontend/app/utils/notifications.js
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-
-export const showErrorMessage = (message) => {
-  toast.error(message);
-};
-
-export const showSuccessMessage = (message) => {
-  toast.success(message);
-};
--- heyama-frontend/app/utils/socket.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-const backEndUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}`;
-
-export const socket = io(backEndUrl, {
-  withCredentials: true,
-  autoConnect: true,
-  transports: ["websocket"],
-});
